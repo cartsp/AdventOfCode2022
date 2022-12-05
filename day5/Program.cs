93013392c1d0ae0f3f62a2538a529e63fe514c58
Liam Boyle <ltboyle@gmail.com>
2022-12-05 16:06:38 +0000

    Mini tidyup

--- day5/Program.cs
+++ day5/Program.cs
@@ -10,10 +10,10 @@ for (var index = 0; index < instructions.Count; index++)
     var order = instructions[index];
     var rangeToMove = stacks[order[1]-1].GetRange(stacks[order[1]-1].Count - order[0], order[0]);
     stacks[order[1]-1].RemoveRange(stacks[order[1]-1].Count - order[0], order[0]);
-    //rangeToMove.Reverse();
+    //rangeToMove.Reverse(); //uncomment for part 1
     stacks[order[2]-1].AddRange(rangeToMove);
 }
 
-var c = stacks.Select(a => a.Last());
+var answer = stacks.Select(a => a.Last().ToString()).Aggregate((current, next) => $"{current}{next}");
 
-var b = 1;
+Console.WriteLine(answer);
