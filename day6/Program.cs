1eed0feb783e75ceb8511417dc20e7f5841011de
Liam Boyle <ltboyle@gmail.com>
2022-12-06 11:19:07 +0000

    Day 6 complete

---
+++ day6/Program.cs
@@ -0,0 +1,23 @@
+﻿var signal = File.ReadAllText("day6input.txt");
+
+var signalArray = signal.ToCharArray();
+
+var part1 = DistinctPoisition(4);
+Console.WriteLine($"Part 1: {part1}");
+
+var part2 = DistinctPoisition(14);
+Console.WriteLine($"Part 2: {part2}");
+
+int DistinctPoisition(int range)
+{
+    for (int i = 0; i < signalArray.Length; i++)
+    {
+        var distinctSignalCount = signal.Skip(i).Take(range).Distinct().Count();
+        if (distinctSignalCount == range)
+        {
+            return i + range;
+        }
+    }
+
+    return 0;
+}
